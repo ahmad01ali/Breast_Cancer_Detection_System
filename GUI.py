8a5181516cc94d92af28cf8a78a3e1e3e9bb14b3
Vaishnavi Lugade <108423518+vaishnavilugade@users.noreply.github.com>
2023-07-09 20:42:20 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,8 +1,11 @@
+import time
 import tkinter as tk
 from tkinter import filedialog
 from PIL import ImageTk, Image
 import numpy as np
 from tensorflow.keras.models import load_model
+import pyttsx3
+from PIL import ImageEnhance, ImageChops
 
 # Load the trained model
 model = load_model('breast_cancer_model.h5')
@@ -18,14 +21,20 @@ def open_image():
     if file_path:
         img = Image.open(file_path)
         img = img.resize((64, 64))  # Resize to the input shape required by the model
-        img_array = np.array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array = img_array.astype('float32') / 255.0
 
-        img = ImageTk.PhotoImage(img)
-        image_label.configure(image=img)
-        image_label.image = img
-        result_label.config(text="")  # Reset the prediction result
+        if is_colorful_image(img):
+            show_error_message("No valid image selected.")
+        else:
+            img_array = np.array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            img_array = img_array.astype('float32') / 255.0
+
+            img = ImageTk.PhotoImage(img)
+            image_label.configure(image=img)
+            image_label.image = img
+            result_label.config(text="")  # Reset the prediction result
+    else:
+        show_error_message("No valid image selected.")
 
 
 # Function to predict the image
@@ -38,21 +47,43 @@ def predict_image():
         aspect_ratio = width / height
 
         # Check aspect ratio to determine if it resembles the shape of a breast
-        if aspect_ratio >= 0.8 and aspect_ratio <= 1.2:
+        if 0.8 <= aspect_ratio <= 1.2:
             pred = model.predict(img_array)
             threshold = 0.5
             class_label = "No Cancer" if pred[0][0] >= threshold else "Cancer"
-            result_label.config(text=f" {class_label} Detected")
+            result_label.config(text=f"{class_label} Detected", fg="red" if class_label == "Cancer" else "green")
 
-            # Change text color based on prediction result
-            if class_label == "Cancer":
-                result_label.config(fg="red")
-            else:
-                result_label.config(fg="green")
+            # Read the result using text-to-speech
+            read_result(class_label)
         else:
-            result_label.config(text="Image does not resemble a breast.", fg="black")
+            show_error_message("No valid image selected.")
     else:
-        result_label.config(text="No valid image selected.", fg="black")
+        show_error_message("No valid image selected.")
+
+
+
+# Function to read the result using text-to-speech
+def read_result(class_label):
+    engine = pyttsx3.init()
+
+    # Set the voice to a female voice
+    voices = engine.getProperty('voices')
+    female_voice = next((voice for voice in voices if "female" in voice.name.lower()), None)
+    if female_voice:
+        engine.setProperty('voice', female_voice.id)
+
+    # Adjust the speaking rate
+    engine.setProperty('rate', 150)  # Adjust the value as needed
+
+    if img_array is not None and img_array.shape == (1, 64, 64, 3):
+        img = Image.fromarray(img_array[0].astype('uint8'))  # Convert array back to Image object
+        width, height = img.size
+        aspect_ratio = width / height
+        engine.say("Breast cancer detected." if class_label == "Cancer" else "No breast cancer detected.")
+    elif img_array is not None and img_array.shape !=(1, 64, 64, 3):
+        engine.say("Invalid image selected for unknown result")
+    engine.runAndWait()
+
 
 
 # Function to remove the image
@@ -63,6 +94,19 @@ def remove_image():
     result_label.config(text="")
 
 
+# Function to check if the image is considered colorful
+def is_colorful_image(img):
+    color_enhancer = ImageEnhance.Color(img)
+    color_enhanced = color_enhancer.enhance(2.0)  # Enhance color
+    difference = ImageChops.difference(img, color_enhanced)
+    return difference.getbbox() is not None
+
+
+# Function to show error messages
+def show_error_message(message):
+    result_label.config(text=message, fg="black")
+
+
 # Create the main window
 window = tk.Tk()
 window.title("Breast Cancer Detection")
@@ -70,11 +114,11 @@ window.geometry("400x550")  # Increased the height to accommodate larger images
 window.configure(bg="#d6cadd")
 
 # Create title label with blue color (hexadecimal code: #236B8E)
-title_label = tk.Label(window, text="Welcome To Breast Cancer Detection ", font=("Arial", 16), fg="#990000" ,bg="#d6cadd")
+title_label = tk.Label(window, text="Welcome To Breast Cancer Detection", font=("Arial", 16), fg="#990000", bg="#d6cadd")
 title_label.pack(pady=10)
 
 # Create image label
-image_label = tk.Label(window,bg="#d6cadd")
+image_label = tk.Label(window, bg="#d6cadd")
 image_label.pack(pady=10)
 
 # Create button to open the image
@@ -90,7 +134,7 @@ predict_button = tk.Button(window, text="CHECK", command=predict_image, bg="#a1c
 predict_button.pack(pady=10)
 
 # Create label to display the prediction result
-result_label = tk.Label(window, text="", font=("Arial", 14),bg="#d6cadd")
+result_label = tk.Label(window, text="", font=("Arial", 14), bg="#d6cadd")
 result_label.pack(pady=10)
 
 # Run the GUI main loop
